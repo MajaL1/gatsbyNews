e9e31f2d1e1bb8a17daf1eea13c38a89519c8f07
MajaL1 <mlokar@triglav.si>
2019-11-06 05:03:56 +0100

    test

---
+++ .cache/async-requires.js
@@ -0,0 +1,19 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-galerija-js": () => import("../src/pages/galerija.js" /* webpackChunkName: "component---src-pages-galerija-js" */),
+  "component---src-pages-homepage-js": () => import("../src/pages/homepage.js" /* webpackChunkName: "component---src-pages-homepage-js" */),
+  "component---src-pages-index-js": () => import("../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-kontakt-js": () => import("../src/pages/kontakt.js" /* webpackChunkName: "component---src-pages-kontakt-js" */),
+  "component---src-pages-operativa-js": () => import("../src/pages/operativa.js" /* webpackChunkName: "component---src-pages-operativa-js" */),
+  "component---src-pages-page-2-js": () => import("../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
+  "component---src-pages-povezave-js": () => import("../src/pages/povezave.js" /* webpackChunkName: "component---src-pages-povezave-js" */),
+  "component---src-pages-pozarninacrt-js": () => import("../src/pages/pozarninacrt.js" /* webpackChunkName: "component---src-pages-pozarninacrt-js" */),
+  "component---src-pages-voznipark-js": () => import("../src/pages/voznipark.js" /* webpackChunkName: "component---src-pages-voznipark-js" */),
+  "component---src-pages-zgodovina-js": () => import("../src/pages/zgodovina.js" /* webpackChunkName: "component---src-pages-zgodovina-js" */)
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,81 +1,46 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { Link, graphql, Image } from 'gatsby'
 import Layout from '../components/layout'
 import {Router, Link as LinkRouter} from '@reach/router'
 import "../css/main.css"
 
 const publishDate = "Objavljeno: 29.10.2019"
 
-const IndexPage = ({data, test="test" }) => (
-  <Layout>
-    <p className="content-title">Aktualno</p>
-    <ul>
-      {data.allStrapiArticle.edges.map(document => (
-       
-        <li className="news-title" key={document.node.id}>
-          <span className="news-date">Objavljeno: {document.node.created_at}, </span>
-          <span className="news-date">{document.node.author.username}</span>
-          
-          <h5 className="news-header3" >
-            <Link className="news-link" to={`/${document.node.id}`}>{document.node.title}</Link>
-          </h5>
-         
-          <span className="news-date">{test}</span>
-          <p className="news-content">{document.node.content}</p>
-          <hr/>
-        </li>
-      ))}
-    </ul>
-  </Layout>
-)
-const Invoice = props => (
-  <div>
-    <h2>Invoice {props.invoiceId}</h2>
-    <Link to="details">Deets</Link>
-    <Router>
-      <InvoiceDetails path="/details" />
-    </Router>
-  </div>
-)
+// In your index.js file
 
-const InvoiceDetails = props => <div>some deets</div>
+const IndexPage = (props) => {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  return (
+    <Layout>
 
-const Invoices = props => (
-  <div>
-    <h2>Invoices</h2>
-    <ul>
-      <li>
-        <Link to="123">Invoice 123</Link>
-      </li>
-      <li>
-        <Link to="abc">Invoice ABC</Link>
-      </li>
-    </ul>
-
-    {props.children}
-  </div>
-)
-
-const InvoicesIndex = () => (
-  <div>
-    <p>Maybe put some pretty graphs here or something.</p>
-  </div>
-)
+      <h1>{data.title}</h1>
+      <p>{data.intro}</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
 
-export const pageQuery = graphql`  
-  query IndexQuery {
-    allStrapiArticle {
+
+// In your index.js file
+
+export const query = graphql`
+  query {
+    allFile (filter: {sourceInstanceName: {eq: "content"} name: {eq: "home"}}) {
       edges {
         node {
-          id
-          title
-          content
-          created_at(formatString: "DD.MM. YYYY")
-          author {username}
+          childMarkdownRemark {
+            frontmatter {
+              title
+              intro
+              image
+          }
         }
       }
     }
   }
-`
+}`
