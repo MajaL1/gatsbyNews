24fdc577b817dc821fb5d918fef370a845a279a3
MajaL1 <mlokar@triglav.si>
2019-11-10 12:35:10 +0100

    test

--- content/home.md
+++ content/home.md
@@ -1,5 +1,15 @@
 ---
-title: '99999'
+title: '22222'
+intro: '999999'
+image: /assets/cave.png
+---
+---
+title: '000000'
+intro: '999999'
+image: /assets/cave.png
+---
+---
+title: '111111'
 intro: '999999'
 image: /assets/cave.png
 ---
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,62 @@ import Layout from '../components/layout'
 import "../css/main.css"
 
 const IndexPage = (props) => {
-  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  //const data = props.data;//.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  //const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  
+  const posts = props.data.allMarkdownRemark.edges
+
   return (
     <Layout>
 
-      <h1>{data.title}</h1>
-      <p>{data.intro}</p>
+
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         
+      {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <article key={node.fields.slug}>
+              <header>
+                <h3
+                >
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </section>
+            </article>
+          )
+        })}
+
+
+
+      <ul>
+      
+   {/*    {data.allFile.edges.map(document => (
+       
+        <li className="news-title" key={document.node.id}>
+          <p className="news-content">test {document.childMarkdownRemark}</p>
+          <span className="news-date">Objavljeno: {document.node.created_at}, </span>
+          
+          <h5 className="news-header3" >
+            <Link className="news-link" to={`/${document.node.id}`}>{document.node.title}</Link>
+          </h5>
+         
+          
+          <p className="news-content">{document.node.content}</p>
+          <hr/>
+        </li>
+      ))} */}
+      
+      </ul>
       </div>
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
@@ -20,10 +68,32 @@ const IndexPage = (props) => {
 
 export default IndexPage
 
+export const pageQuery = graphql`
+query {
+  site {
+    siteMetadata {
+      title
+    }
+  }
+  allMarkdownRemark {
+    edges {
+      node {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+         
+          title
+          intro
+        }
+      }
+    }
+  }
+}
+`
 
-// In your index.js file
-
-export const query = graphql`
+/* export const query = graphql`
   query {
     allFile (filter: {sourceInstanceName: {eq: "content"} name: {eq: "home"}}) {
       edges {
@@ -38,4 +108,4 @@ export const query = graphql`
       }
     }
   }
-}`
+}` */
