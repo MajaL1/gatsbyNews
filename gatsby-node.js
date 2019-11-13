03171bc4cfcf3969890bc34ca4a87ef00d9e4215
MajaL1 <MajaL1@users.noreply.github.com>
2019-11-13 14:07:46 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -68,8 +68,3 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   }
 }
 
-exports.onCreatePage = async ({ page, actions }) => {     
-    const { createPage } = actions     
-    page.matchPath = "/*/*"     
-    createPage(page) 
-}
